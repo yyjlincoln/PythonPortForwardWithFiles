af94655a8b05e9e542c1cb3f0fa222632a4baf7f
yyjlincoln <yyjlincoln@gmail.com>
2018-07-31 08:14:55 +1000

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
-import SuproMain as s
+import SuMa as s
 import time
 print('Client')
-s.client(('localhost',int(input('Please enter the server port>'))),('localhost',81))
+s.client(('localhost',int(input('Please enter the server port>'))),('localhost',81),id=str(input('Please input an id>')))
 while 1:
     time.sleep(1000)
