44f0d3d3b1f9f9e47cd71c069ceccf229b314da1
yyjlincoln <yyjlincoln@gmail.com>
2018-07-31 08:14:28 +1000

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 import SuproMain as s
 import time
 print('Server')
-s.server(('localhost',int(input('Please input the server port>'))))
+s.server(('localhost',int(input('Please input the server port>'))),id=str(input('Please input an id (same as the server)>')))
 while 1:
     time.sleep(1000)
